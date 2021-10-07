1f19d03b9fb39770168bb19ff9b54dc78e5d543a
TerraBoii <kostyaborisenko05@gmail.com>
2021-10-07 16:28:59 +0300

    created rules for generating seed

--- generators.py
+++ generators.py
@@ -16,7 +16,7 @@ def getCursorPosition():
     return { "x": pt.x, "y": pt.y}
 
 
-def create_seed(app_version: str, app_x, app_y, screen_x, screen_y) -> float:
+def create_seed(app_version: str, app_width: int, app_height: int, screen_width: int, screen_height: int) -> float:
     data = None
     # Getting application version from internet page, can be the same as installed one
     try:
@@ -29,23 +29,23 @@ def create_seed(app_version: str, app_x, app_y, screen_x, screen_y) -> float:
     x, y = cursor_position['x'], cursor_position['y']
     # Rule 1 for generating seed: getting cursor position and manipulating with x, y values
     rule_1 = ((((int(str(x + 1) + str(y + 1)) * 2) // ((x + 1 * y + 1) + 1)) * (x + 1 + y + 1)) - (x + y)) * (x + 1 + y + 1)
-    print(f"rule_1 = {rule_1}")
     sleep(0.001)  # stoping program for 1 millisecond to get different time number
     # Rule 2: getting and manipulating with time
     rule_2 = int(''.join(str(time()).split('.')))
-    print(f"rule_2 = {rule_2}")
     # Rule 3: manipulating with app versions
     rule_3 = int((''.join(app_version.split('.'))) + (''.join(data.split('.')))) * \
              int((''.join(app_version.split('.'))) + (''.join(data.split('.'))))
-    print(f"rule_3 = {rule_3}")
-    rule_4 = ...
-    print(f"rule_4 = {rule_4}")
-    new_seed = (rule_1 * rule_3) + rule_2
+    # Rule 4: manipulating with screen size data and window size
+    dif_x = screen_width - app_width + 1
+    dif_y = screen_height - app_height + 1
+    rule_4 = (((((screen_width // dif_x) * app_width) + ((screen_height // dif_y) * app_height)) + \
+            (screen_width + screen_height + dif_x + dif_y + app_width + app_height)))
+    new_seed = ((rule_1 * rule_3) + rule_2) * rule_4
     return new_seed
 
 
-def perimeter_task(figure: str, app_version, app_x, app_y):
-    new_seed = create_seed(app_version, app_x, app_y)
+def perimeter_task(figure: str, app_version: str, app_width: int, app_height: int, screen_width: int, screen_height: int):
+    new_seed = create_seed(app_version, app_width, app_height, screen_width, screen_height)
     seed(new_seed)
     if figure == 'square_task':
         return randint(2, 100)
@@ -55,8 +55,8 @@ def perimeter_task(figure: str, app_version, app_x, app_y):
         return a, b
 
 
-def square_task(figure: str, app_version, app_x, app_y):
-    new_seed = create_seed(app_version, app_x, app_y)
+def square_task(figure: str, app_version: str, app_width: int, app_height: int, screen_width: int, screen_height: int):
+    new_seed = create_seed(app_version, app_width, app_height, screen_width, screen_height)
     seed(new_seed)
     if figure == 'square_task':
         return randint(2, 100)
@@ -65,5 +65,7 @@ def square_task(figure: str, app_version, app_x, app_y):
         b = randint(5, 75) + (a//2) + 1
         return a, b
 
-print(create_seed("0.1", 100, 200))
+print(create_seed("0.1", 800, 600, 1920, 1080))
+print(create_seed("0.1", 800, 600, 1920, 1080))
+print(create_seed("0.1", 800, 600, 1920, 1080))
 print('look at me')
