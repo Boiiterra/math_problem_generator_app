f7680890d8598b62c38c2fd02974286f13ca9b9b
TerraBoii <kostyaborisenko05@gmail.com>
2021-10-28 17:19:11 +0300

    changed return options

--- generators.py
+++ generators.py
@@ -47,19 +47,25 @@ def perimeter_task(figure: str, app_version: str, app_width: int, app_height: in
     new_seed = __create_seed(app_version, app_width, app_height, screen_width, screen_height)
     seed(new_seed)
     if figure == 'square':
-        return randint(2, 100), new_seed
+        side = randint(2, 100)
+        answer = 4 * side
+        return side, answer, new_seed
     elif figure == 'rectangle':
-        a = randint(10, 120)
-        b = randint(5, 75) + (a//2) + 1
-        return a, b, new_seed
+        height = randint(10, 120)
+        width = randint(5, 75) + (height // 2) + 1
+        answer = 2 * (height + width)
+        return height, width, answer, new_seed
 
 
 def area_task(figure: str, app_version: str, app_width: int, app_height: int, screen_width: int, screen_height: int):
     new_seed = __create_seed(app_version, app_width, app_height, screen_width, screen_height)
     seed(new_seed)
     if figure == 'square':
-        return randint(2, 100), new_seed
+        side = randint(2, 100)
+        answer = 4 * side
+        return side, answer, new_seed
     elif figure == 'rectangle':
-        a = randint(10, 120)
-        b = randint(5, 75) + (a//2) + 1
-        return a, b, new_seed
+        height = randint(10, 120)
+        width = randint(5, 75) + (height//2) + 1
+        answer = height * width
+        return height, width, answer, new_seed
