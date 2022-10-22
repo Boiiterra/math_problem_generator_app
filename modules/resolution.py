8e804c56f38c32e5e303c4e0cea065debf4867be
TerraBoii <kostyaborisenko05@gmail.com>
2022-10-22 19:51:37 +0300

    Good sys for font changing based on app, maybe...

--- modules/resolution.py
+++ modules/resolution.py
@@ -1,3 +1,5 @@
-def scale(width: int, height: int) -> float:
-    print(((width / 950) + (height / 650)) / 2)
-    return ((width / 950) + (height / 650)) / 2
+def scale(width: int, height: int, previous: int | float) -> float:
+    if width / 950 <= height / 650 <= (width / 950) * 1.3 or height / 650 <= width / 950 <= (height / 650) * 1.3:
+        return ((width / 950) + (height / 650)) / 2
+    else:
+        return previous
