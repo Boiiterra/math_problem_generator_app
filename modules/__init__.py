f01dd015bfd0a7e775cbc65db62ad54ae751a454
TerraBoii <kostyaborisenko05@gmail.com>
2022-11-08 18:03:35 +0300

    sorted for some automation :)

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,3 +1,3 @@
-from .generators import perimeter_task, area_task, square_equation, arithmetics
-from .tooltip import create_tool_tip
 from .resolution import scale
+from .tooltip import create_tool_tip
+from .generators import perimeter_task, area_task, square_equation, arithmetics
