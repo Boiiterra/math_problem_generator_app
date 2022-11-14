3c83ba05a4fd70b7aa5089522b99458023edc1df
TerraBoii <kostyaborisenko05@gmail.com>
2022-11-14 13:42:31 +0300

    Scaling scale fix #32

--- modules/resolution.py
+++ modules/resolution.py
@@ -2,4 +2,7 @@ def scale(width: int, height: int, previous: int | float) -> float:
     if width / 950 <= height / 650 <= (width / 950) * 1.3 or height / 650 <= width / 950 <= (height / 650) * 1.3:
         return ((width / 950) + (height / 650)) / 2
     else:
-        return previous
+        if height > width:
+            return width / 950
+        else:
+            return height / 650
