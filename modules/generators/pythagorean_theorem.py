4e8ceac43df71a77dae29fa2dccf30f9cda3da45
TerraBoii <kostyaborisenko05@gmail.com>
2022-11-15 19:50:58 +0300

    :P

--- modules/generators/pythagorean_theorem.py
+++ modules/generators/pythagorean_theorem.py
@@ -2,6 +2,7 @@ from random import seed, randint
 
 from .c_seed import create_seed
 
+
 def pythagorean_theorem(app_version: str, app_width: int, app_height: int, screen_width: int, screen_height: int):
 	new_seed = create_seed(app_version, app_width, app_height, screen_width, screen_height)
 	seed(new_seed)
